fc39ffa529ddc5fa00896996585e3c1469c1ef14
Jakub Susz <56273076+Arut3nCodes@users.noreply.github.com>
2024-10-09 16:17:35 +0200

    Dodano WorkerModel.java i wstępną strukturę projektu

---
+++ src/main/java/com/zpi/fryzland/model/WorkerModel.java
@@ -0,0 +1,11 @@
+package com.zpi.fryzland.model;
+
+import jakarta.persistence.Entity;
+
+//@Entity(name="Pracownicy")
+////public class EmployeeModel {
+////    private int employeeID;
+////    private String employeeName;
+////    private String employeeSurname;
+////    private String
+////}
