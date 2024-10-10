8b10ade6cb007f93389d964d55ce5ce7d048deea
Freshh0 <remeksz10@gmail.com>
2024-10-10 18:45:55 +0200

    Uzupełniono modele kwalifikacji pracowników i kategorii usługi

--- src/main/java/com/zpi/fryzland/model/EmployeeQualificationModel.java
+++ src/main/java/com/zpi/fryzland/model/EmployeeQualificationModel.java
@@ -1,4 +1,25 @@
 package com.zpi.fryzland.model;
 
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "KwalifikacjePracownika")
 public class EmployeeQualificationModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "KwalifikacjaID")
+    private Integer employeeQualificationID;
+    @ManyToOne
+    @JoinColumn(name = "KategoriaID")
+    private ServiceCategoryModel serviceCategoryID;
+    @ManyToOne
+    @JoinColumn(name = "PracownikID")
+    private EmployeeModel employeeID;
+
 }
--- src/main/java/com/zpi/fryzland/model/ServiceCategoryModel.java
+++ src/main/java/com/zpi/fryzland/model/ServiceCategoryModel.java
@@ -1,4 +1,21 @@
 package com.zpi.fryzland.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "KategoriaUslugi")
 public class ServiceCategoryModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "KategoriaID")
+    private Integer serviceCategoryId;
+    @Column(name = "NazwaKat")
+    private String categoryName;
+    @Column(name = "OpisKat")
+    private String categoryDescription;
 }
