d645d6413a406887f5e758a364dd14082a6863ea
Jakub Susz <56273076+Arut3nCodes@users.noreply.github.com>
2024-11-19 17:54:00 +0100

    Dodanie nowych typów wiadomości i poprawne wysłanie maila

--- src/main/java/com/zpi/fryzland/service/EmailService.java
+++ src/main/java/com/zpi/fryzland/service/EmailService.java
@@ -1,6 +1,5 @@
 package com.zpi.fryzland.service;
 
-import com.zpi.fryzland.controller.MailConfigChecker;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,22 +8,20 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class EmailService {
-
     private JavaMailSender mailSender;
-    private MailConfigChecker mailConfigChecker;
     public enum EmailType{
         TEST_EMAIL,
         REGISTER,
         PASSWORD_LOST,
+        PASSWORD_CHANGED,
         VISIT_RESERVED,
         VISIT_REMINDER,
         VISIT_CUSTOMER_CANCELLED,
         VISIT_EMPLOYEE_CANCELLED,
-        VISIT_CHANGED
+        VISIT_DATE_OR_TIME_CHANGED,
     }
 
     public void sendEmail(String to, String subject, EmailType emailType) {
-        mailConfigChecker.logMailConfig();
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
