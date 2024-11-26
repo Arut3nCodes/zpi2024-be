e2ad2956cc874d2a8e1374e15234820ea31eb760
Freshh0 <remeksz10@gmail.com>
2024-11-26 16:46:18 +0100

    Poprawiono nazwę kolumny w tabeli Wizyty

--- src/main/java/com/zpi/fryzland/model/VisitModel.java
+++ src/main/java/com/zpi/fryzland/model/VisitModel.java
@@ -35,7 +35,7 @@ public class VisitModel {
     @Column(name="StatusW", nullable = false)
     private VisitStatus visitStatus;
     @ManyToOne
-    @JoinColumn(name="PrzydziałDoSalonuID")
+    @JoinColumn(name="PrzydzialDoSalonuID")
     private AssignmentToSalonModel assigmentModel;
     @ManyToOne
     @JoinColumn(name="KlientID")
