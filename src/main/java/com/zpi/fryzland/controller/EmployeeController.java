5da3c6d0e2793f374c502a0cf30d13acc8068d13
Freshh0 <remeksz10@gmail.com>
2024-12-07 15:14:01 +0100

    Dodano endpoint pobierania pracownika po id

--- src/main/java/com/zpi/fryzland/controller/EmployeeController.java
+++ src/main/java/com/zpi/fryzland/controller/EmployeeController.java
@@ -17,10 +17,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -70,6 +67,21 @@ public class EmployeeController {
             }
             else{
                 return ResponseEntity.noContent().build();
+            }
+        }catch(Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{employeeID}")
+    public ResponseEntity<EmployeeDTO> getEmployeeByID(@PathVariable int employeeID){
+        try{
+            Optional<EmployeeModel> employeeModel = employeeService.getEmployeeById(employeeID);
+            if(employeeModel.isPresent()){
+                return ResponseEntity.ok(employeeMapper.toDTO(employeeModel.get()));
+            }
+            else{
+                return ResponseEntity.notFound().build();
             }
         }catch(Exception e){
             return ResponseEntity.badRequest().build();
