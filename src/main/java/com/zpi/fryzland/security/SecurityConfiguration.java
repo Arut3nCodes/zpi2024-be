41807ac1d1d2879e1571d15495c9437e8c26b621
Jakub Susz <56273076+Arut3nCodes@users.noreply.github.com>
2024-12-09 13:17:47 +0100

    Zmiany kosmetyczne

--- src/main/java/com/zpi/fryzland/security/SecurityConfiguration.java
+++ src/main/java/com/zpi/fryzland/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -46,9 +47,9 @@ public class SecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((authorize) -> authorize
                         //todo: Zmienic role na admina
-//                        .requestMatchers("/api/auth/employee/register").permitAll()
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .requestMatchers(HttpMethod.GET,"/api/crud/salons").permitAll()
+                        .requestMatchers("/api/auth/employee/register").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/crud/salons").permitAll()
                         .requestMatchers("**").permitAll()
                         .anyRequest().authenticated()
                 )
